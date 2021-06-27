f70b1ef5689c036f392e4810332f5d3f7cb9a173
Engelbert Estrada <engelbertestrada201930200@cunoc.edu.gt>
2021-06-27 02:33:34 -0600

    Commit 1 portada juego

---
+++ src/bienvenida/Presentar.java
@@ -0,0 +1,7 @@
+
+package bienvenida;
+
+
+public class Presentar {
+    
+}
---
+++ src/principal/Principal.java
@@ -0,0 +1,14 @@
+
+package principal;
+import bienvenida.*;
+
+
+public class Principal {
+    
+    public static void main(String[] args) {
+        Inicio inicio = new Inicio();
+        inicio.setVisible(true);
+    }
+    
+    
+}
